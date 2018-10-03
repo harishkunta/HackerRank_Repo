0c4b5a90f99f75ebca13cd12699a42b84ab82afa
Harish Kunta <harish.k@techaspect.com>
2018-10-03 23:41:18 +0530

    Added Source for queue using two stacks

---
+++ Day_7/queueusingtwostacks.js
@@ -0,0 +1,37 @@
+'use strict';
+const fs = require('fs');
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let _input = "";
+let currentLine = 0;
+function readLine() {
+    return _input[currentLine++];
+}
+
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", _ => {
+    _input = _input.trim().split('\n').map(str => str.trim());
+   processData(_input);
+});
+function processData(input) {
+    //Enter your code here
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    var nq = parseInt(readLine(), 10);
+    var arr=[];
+    for(let i=0;i<nq;i++) {
+        var q = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+        if(q[0]==1) {
+            arr.push(q[1]);
+        }
+        else if(q[0]==2) {
+            arr.shift();
+        }
+        else {  
+                ws.write(arr[0]+"\n");
+            }
+        
+}
+} 
