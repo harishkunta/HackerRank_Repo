5fbbcc7a11a5b003ab04e12d7b57fd549cfdc489
Harish Kunta <harish.k@tadigital.com>
2018-10-08 10:49:59 +0530

    Added Source for viral advertising

---
+++ Day_11/viraladvertising.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the viralAdvertising function below.
+function viralAdvertising(n) { 
+    var shared=5, liked=2, cumulative = 2;
+    for(var i=1;i<n;i++) {
+        shared=liked*3;
+        liked=(Math.floor(shared/2));
+        cumulative = cumulative+liked;
+    }
+    return cumulative;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let result = viralAdvertising(n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
